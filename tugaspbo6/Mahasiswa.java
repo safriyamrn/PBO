dc8ed5f5bbe883ba22202c30d438368977c56caa
safriyamrn <148854440+safriyamrn@users.noreply.github.com>
2023-11-05 14:29:22 +0700

    Add files via upload

---
+++ tugaspbo6/Mahasiswa.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tugaspbo6;
+
+/**
+ *
+ * @author Safriya Murni
+ */
+public class Mahasiswa {
+    private String nim;
+    private String nama;
+    private String jenis_kelamin;
+    private String semester;
+    private String alamat;
+
+    /**
+     * @return the nim
+     */
+    public String getNim() {
+        return nim;
+    }
+    /**
+     * @param nim the nim to set
+     */
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+    /**
+     * @return the nama
+     */
+    public String getNama() {
+        return nama;
+    }
+    /**
+     * @param nama the nama to set
+     */
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+    /**
+     * @return the jenis_kelamin
+     */
+    public String getJenis_kelamin() {
+        return jenis_kelamin;
+    }
+    /**
+     * @param jenis_kelamin the jenis_kelamin to set
+     */
+    public void setJenis_kelamin(String jenis_kelamin) {
+        this.jenis_kelamin = jenis_kelamin;
+    }
+    /**
+     * @return the semester
+     */
+    public String getSemester() {
+        return semester;
+    }
+    /**
+     * @param semester the semester to set
+     */
+    public void setSemester(String semester) {
+        this.semester = semester;
+    }
+    /**
+     * @return the alamat
+     */
+    public String getAlamat() {
+        return alamat;
+    }
+    /**
+     * @param alamat the alamat to set
+     */
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+}
